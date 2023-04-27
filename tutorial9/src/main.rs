e465eebdbde25af6ca989da01082afc8b688b47a
¨ValeCrafter¨ <valerio04@gmx.ch>
2023-04-28 00:00:26 +0200

    Added  tutorial9

---
+++ tutorial9/src/main.rs
@@ -0,0 +1,23 @@
+use core::num;
+
+fn main() {
+    for i in 1..11 {
+        println!("i is {}", i);
+    }
+
+    let numbers = 30..51;
+
+    for i in numbers {
+        println!("The number is {}", i);
+    }
+
+    let animals = vec!["Vogel", "Hurensohn", "Jenny"];
+
+    for a in animals.iter() {
+        println!("{} is an animal.", a);
+    }
+
+    for (index, a) in animals.iter().enumerate(){
+        println!("{} in the list is {}",index, a);
+    }
+}
